18d5a35e1bbe75156f9b91688b70bdbabf63c117
Dayannis-PC\Dayannis <dayannis.y@gmail.com>
2018-03-12 15:52:54 -0400

    8. Consulta de POST por Categoría y Etiquetas

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,8 @@ Route::redirect('/', 'blog');
 
 Auth::routes();
 
-Route::get('/blog', 'Web\PageController@blog')->name('blog');
+//web
+Route::get('blog', 'Web\PageController@blog')->name('blog');
 Route::get('entrada/{slug}',  'Web\PageController@post')->name('post');
+Route::get('categoria/{slug}','Web\PageController@category')->name('category');
+Route::get('etiqueta/{slug}', 'Web\PageController@tag')->name('tag');
